f1954ddb748aa41847ef134289f6a7ebaf73770e
Sarthak Jariwala <jariwala@uw.edu>
2020-06-26 16:11:39 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='PublishScience',
+    version='0.1',
+    py_modules=['myscript'],
+    install_requires=[
+        'Click',
+    ],
+    entry_points='''
+        [console_scripts]
+        pbs=myscript:main
+    ''',
+)
